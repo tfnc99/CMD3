801270d94fed4f5a7de0ec9c1f4f5dbef4f87f29
tfnc99 <tfortrannc@gmail.com>
2015-01-08 10:15:23 -0800

    Added CLEAR back in
    I decided to make it so that cmd3std is compiled with MinGW so I can
    just use system() with Windows directly. Change it to clear on
    Unix/Linux, it'll still work perfectly!
    (btw why are 64-Bit MinGW binaries so big??)

--- plugins/cmd3std/main.cpp
+++ plugins/cmd3std/main.cpp
@@ -1,9 +1,22 @@
+#include <cstdlib>
 #include "cmd3.h"
 
+/** Command: ECHO [Stuff]
+*       Prints [Stuff] back into printer.
+*/
 int com_echo(std::string* args, std::ostream& printer)
 {
     printer<<args[MAX_ARG_COUNT]; //The user pressing enter adds a newline for us
     return 0;
+}
+
+/** Command: CLEAR [None]
+*       Clears the screen, because cygwin had issues with system() when it wasn't a
+*       full installation I just decided to move this to a plugin compiled with MinGW.
+*/
+int com_clear(std::string* args, std::ostream& printer)
+{
+    return system("cls");
 }
 
 extern "C"
@@ -15,6 +28,11 @@ extern "C"
         current->command = "echo";
         current->description = "ECHO [Stuff] :: Prints stuff";
         current->to_call = &com_echo;
+        current->next = new plugin_command_s;
+        current = current->next;
+        current->command = "clear";
+        current->description = "CLEAR [None] :: Clears the screen";
+        current->to_call = &com_clear;
         current->next = NULL;
         return root;
     }
